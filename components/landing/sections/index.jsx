8d42002347f412921bab8f2ef7e66a6a7cc54757
Man Chon Kuok <mckuok@referentia.com>
2022-06-09 02:04:26 -1000

    sections responsive

--- components/landing/sections/index.jsx
+++ components/landing/sections/index.jsx
@@ -1,15 +1,27 @@
+import { MD_WIDTH, useWindowDimensions } from "../../../services/windows-size";
 import styles from "../../../styles/Section.module.css";
 
 export default function Section({backgroundUrl, backgroundHeight, leftInfo, rightInfo}) {
+    const windowsDimentions = useWindowDimensions();
+    let height = backgroundHeight;
+    if (windowsDimentions !== null && windowsDimentions.width < MD_WIDTH) {
+        height = 'auto';
+    }
+    
     return (
         <div>
-            <div className={`${styles['bg-container']}`} style={{backgroundImage: `url(${backgroundUrl})`, minHeight: backgroundHeight}}>
-                <div className={`flex flex-row justify-center w-3/5 mx-auto ${styles.content}`}>
-                    <div className={styles['image-container']}>
-                        {leftInfo}
-                    </div>
-                    <div>
-                        {rightInfo}
+            <div className={`${styles['bg-container']}`} style={{height, minHeight: backgroundHeight}}>
+                <div className="absolute w-full h-full">
+                    <img className="max-w-none h-full md:w-full" src={backgroundUrl}/>
+                </div>
+                <div className="flex flex-col justify-center h-full">
+                    <div className={`flex flex-col md:flex-row justify-center w-3/5 mx-auto`}>
+                        <div className={`styles['image-container'] z-10`}>
+                            {leftInfo}
+                        </div>
+                        <div className="z-10">
+                            {rightInfo}
+                        </div>
                     </div>
                 </div>
             </div>
---
+++ services/windows-size.js
@@ -0,0 +1,29 @@
+import { useState, useEffect } from 'react';
+
+export const SM_WIDTH = 640;
+export const MD_WIDTH = 768;
+
+function getWindowDimensions() {
+  const { innerWidth: width, innerHeight: height } = window;
+  return {
+    width,
+    height
+  };
+}
+
+export function useWindowDimensions() {
+  const [windowDimensions, setWindowDimensions] = useState(null);
+
+  useEffect(() => {
+    setWindowDimensions(getWindowDimensions());
+
+    function handleResize() {
+      setWindowDimensions(getWindowDimensions());
+    }
+
+    window.addEventListener('resize', handleResize);
+    return () => window.removeEventListener('resize', handleResize);
+  }, []);
+
+  return windowDimensions;
+}
