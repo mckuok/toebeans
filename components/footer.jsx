f2c515ef1f298cb8dd86a4631fc1db42633d2491
Man Chon Kuok <mckuok@referentia.com>
2022-06-10 00:46:17 -1000

    header responsiveness

--- components/footer.jsx
+++ components/footer.jsx
@@ -3,15 +3,15 @@ import styles from '../styles/Footer.module.css'
 
 export default function Footer() {
     return (
-        <div className={`w-full flex flex-col md:flex-row content-start justify-between py-11 px-16 text-xl ${styles.background}`}>
-            <div>
+        <div className={`w-full flex flex-col lg:flex-row content-start justify-start lg:justify-between py-11 px-16 text-xl gap-y-10 ${styles.background}`}>
+            <div className='hidden lg:block'>
                 <Image src='/logo.png' width={60 * 2.5} height={60 * 2.5}/>
             </div>
             <div>
                 <div className={`${styles.newsletter} text-center`}>
                     <p className="link-text">sign up for our newsletter</p>
                 </div>
-                <div className={`flex flex-row justify-between relative content-center pt-5 px-5 ${styles['icons-color']}`}>
+                <div className={`flex flex-row justify-start lg:justify-between relative content-center pt-8 px-0 lg:px-5 gap-x-14 lg:gap-x-0 ${styles['icons-color']}`}>
                     <Image src={'/footer/facebook.svg'} width={30} height={30}/>
                     <Image src={'/footer/instagram.svg'} width={30} height={30}/>
                     <Image src={'/footer/email.svg'} width={30} height={30}/>
