495487955d45418580549939d00fe8942bebb0ab
Man Chon Kuok <mckuok@referentia.com>
2022-06-11 01:33:27 -1000

    link text style update

--- components/landing/sections/section3/index.jsx
+++ components/landing/sections/section3/index.jsx
@@ -18,12 +18,12 @@ export default function SectionThree() {
             rightInfo={
                 <div>
                     <p className={sectionStyles.title}>Supported by KAT Charities</p>
-                    <p className={sectionStyles.text}><span className='link-text'>KAT Charities</span> was born out of a dream to unite children with disabilities and animals to caring, loving people </p>
+                    <p className={sectionStyles.text}><a className='link-text' href="https://katcharities.org/" target={"_blank"}>KAT Charities</a> was born out of a dream to unite children with disabilities and animals to caring, loving people </p>
                     <br />
                     <p className={sectionStyles.text}>Toe Beans &amp; Dream creates a community centered around love, care and joy where the possibilities are endless</p>
                     <br />
                     <div className={`${styles.donation}`}>
-                        <p className={`link-text`}> Donations </p>
+                        <a className={`link-text`} href="https://www.paypal.com/paypalme/katcharities" target={"_blank"}> Donations </a>
                     </div>
                 </div>
             }
